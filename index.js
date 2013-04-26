08f7e2c988c1e05c4765f7c9298855bffb0a5d83
Brian Dentino <brian.dentino@gmail.com>
2013-04-25 21:57:08 -0700

    supports two players

--- index.js
+++ index.js
@@ -3,25 +3,31 @@
  */
 
 //var io = require('io'),
-var device = require('device')('http://10.0.0.21:9000/baseball'),
+var device = require('device')('http://ws.mat.io:80/baseball'),
     splash = require('socket-splash'),
-    //batterLib = require('./batter'),
+    batterLib = require('./batter'),
     fieldLib = require('./field'),
     pitcherLib = require('./pitcher');
 
 console.log(device);
-//var batter = device('batter','mobile');
-var viewer = device('field','desktop');
-var pitcher = device('pitcher', 'mobile');
+var batter = device('batter','mobile');
+var viewer1 = device('field','desktop');
+//var viewer2 = device('field2','desktop');
+//var viewer3 = device('field3','desktop');
+var pitcher = device('pitcher','mobile');
 
-//batter.ready(batterLib);
-viewer.ready(fieldLib);
+batter.ready(batterLib);
+viewer1.ready(fieldLib);
+//viewer2.ready(fieldLib);
+//viewer3.ready(fieldLib);
 pitcher.ready(pitcherLib);
 
 splash('baseball!')
   .desc('Play Ball!')
-  //.add(batter)
-  .add(viewer)
+  .add(batter)
+  .add(viewer1)
+  //.add(viewer2)
+  //.add(viewer3)
   .add(pitcher)
   .ready()
 
